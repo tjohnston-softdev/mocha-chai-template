5e3214d111f83bad3cce3fedcf09050e0d3c051b
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-11-27 14:09:30 +1000

    Demo Files

--- test/index.js
+++ test/index.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+const mocha = require("mocha");
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("Unit Tests", function()
+{
+	it("Test 1", function()
+	{
+		expect(true).to.be.true;
+	});
+	
+	it("Test 2", function()
+	{
+		expect(true).to.be.true;
+	});
+	
+	it("Test 3", function()
+	{
+		expect(true).to.be.true;
+	});
+	
+});
